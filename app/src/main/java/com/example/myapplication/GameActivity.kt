a638a02c496598df16e270029b0645943abc2d7e
damien <damie@LAPTOP-CFSM6TA7>
2023-03-06 09:51:05 +0200

    Updated Project

--- app/src/main/java/com/example/myapplication/GameActivity.kt
+++ app/src/main/java/com/example/myapplication/GameActivity.kt
@@ -3,10 +3,28 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
 class GameActivity : AppCompatActivity() {
+
+    // Declare an array of strings
+    internal var Superhero = arrayOf(
+        "Batman", "Superman", "Ironman", "Spiderman", "Antman", "Powerman", "Latexman", "Musicman"
+    )
+    // Declare all other variables
+    lateinit var superhero: String
+    lateinit var random: Random
+
+    // Declare variables for the view
+    lateinit var txtQuestionContainer: TextView
+    lateinit var etUserInput: EditText
+    lateinit var btnCheck: Button
+    lateinit var btnNext: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
@@ -15,5 +33,48 @@ class GameActivity : AppCompatActivity() {
         val textMessage = findViewById<TextView>(R.id.txtUName).apply {
             text = message
         }
+
+        txtQuestionContainer = findViewById(R.id.txtQuestionContainer)
+
+        etUserInput = findViewById(R.id.etUserInput)
+
+        btnCheck = findViewById(R.id.btnCheck)
+        btnNext = findViewById(R.id.btnNext)
+
+        // intilize the random variable
+        random = Random()
+
+        // Actual logic
+        superhero = Superhero[random.nextInt(Superhero.size)]
+        txtQuestionContainer.text = mixWords(superhero)
+
+        // Set the Listener to the Check button
+        btnCheck.setOnClickListener{
+            if(etUserInput.text.toString().equals(superhero, ignoreCase = true))
+            {
+                Toast.makeText(this@GameActivity,"You got it right!", Toast.LENGTH_SHORT).show()
+            }
+            else Toast.makeText(this@GameActivity,"You got it wrong!", Toast.LENGTH_SHORT).show()
+        }
+
+        // Set the Listener to the Next button
+        btnNext.setOnClickListener{
+            superhero = Superhero[random.nextInt(Superhero.size)]
+            txtQuestionContainer.text = mixWords(superhero)
+        }
+
     }
-}
+        fun mixWords(word: String) : String
+        {
+            val word = Arrays.asList<String>(*word.split("".toRegex()).dropLastWhile({it.isEmpty()}).toTypedArray())
+
+            Collections.shuffle(word)
+            var mixed = ""
+
+            for (i in word)
+            {
+             mixed += i
+            }
+            return mixed
+        }
+    }
